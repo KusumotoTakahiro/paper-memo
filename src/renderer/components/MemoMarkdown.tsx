5228afba1228ba82bf4e5035464e967501a7f273
KusumotoTakahiro <gantaka921@icloud.com>
2024-01-23 20:54:05 +0900

    [update] 表の作成機能の実装と初回表示画面のバグ修正

--- src/renderer/components/MemoMarkdown.tsx
+++ src/renderer/components/MemoMarkdown.tsx
@@ -13,7 +13,10 @@ import {
   Fade,
   TextField,
   Menu,
+  MenuList,
   MenuItem,
+  Popper,
+  ClickAwayListener,
 } from '@mui/material';
 import FormatBoldIcon from '@mui/icons-material/FormatBold';
 import FormatItalicIcon from '@mui/icons-material/FormatItalic';
@@ -42,6 +45,7 @@ const myTextAreaStyle = {
 };
 
 const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
+  const [init, setInit] = React.useState<boolean>(false);
   const [memoContents, setMemoContents] = React.useState<string>('');
   const [memoHTML, setMemoHTML] = React.useState<string>('');
   const [watchMD, setWatchMD] = React.useState<boolean>(true);
@@ -62,7 +66,6 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
 
     if (selectionStart !== selectionEnd) {
       textarea.setRangeText('', selectionStart, selectionEnd, 'end');
-      console.log(selectionStart);
       setMemoContents(textarea.value);
       textarea.focus();
     } else {
@@ -161,7 +164,6 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
           const line = lines[i];
           new_text += `${tag} ${line}\n`;
         }
-        console.log(selectedText);
       } else {
         new_text = `${tag} \n${tag} \n${tag} \n`;
       }
@@ -209,7 +211,6 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
       let new_text = '';
       if (textSelected) {
         new_text += `<font color="${textColor}">${selectedText}</font>`;
-        console.log(selectedText);
         textarea.setRangeText(new_text, selectionStart, selectionEnd);
         setMemoContents(textarea.value);
         textarea.setSelectionRange(
@@ -225,33 +226,65 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
     setSelectedText('');
   };
 
+  const trimSpace = (x: string) => {
+    return x.replace(/^[\s　]+|[\s　]+$/g, '');
+  };
+
+  /**
+   * 表作成の関数
+   * @param type (string): 表のセルを左，中央，右のどれで揃えるかを指定する．
+   *
+   * <表の作成方針>
+   * (1) テキストの未選択時 → 空の1行3列の表を作成する．
+   * (2) テキスト選択時
+   * 　 (2-1) 1行の時: 単語数をNを算出し，1行N列の表を作成する．
+   * 　 (2-2) 複数行(Ｍ行)の時: 行ごとに単語数を算出し，最大の単語数をNとしたとき，M行N列の表を作成する．
+   *
+   * <単語の算出方針>
+   * 改行コードをもとに行を分割する．
+   * (1行選択の時は改行コードがなく分割されないのでそのまま）
+   *
+   * 1行で前後に空白（半角，全角）を含むものはトリムする．(trimSpace()でトリムは実装している)
+   * トリム済みの行に対して空白（半角，全角）を区切り文字に行を単語に分割する．
+   * ※ 空白を区切り文字に指定している理由は
+   * 　 文章をセルに入れたい場合にカンマだと意図しない区切りが発生するから．
+   */
   const drawTable = (type: string) => {
     let textarea = document.getElementById('tarea') as HTMLTextAreaElement;
-    console.log(selectedText);
     if (textarea) {
       const selectionStart = textarea.selectionStart;
       const selectionEnd = textarea.selectionEnd;
       let new_text = `|`;
+      let lines = [];
+      let words = [];
       if (textSelected) {
-        const lines = selectedText.split('\n');
+        lines = trimSpace(selectedText).split('\n');
+        // 行ごとに単語数を数えて最も単語数で表を作成する．
+        let max_words = 0;
+        for (let i = 0; i < lines.length; i++) {
+          words = trimSpace(lines[0]).split(/\s+/);
+          if (max_words < words.length) max_words = words.length;
+        }
         // Tableのheader生成
-        for (let i = 0; i < lines[0].length; i++) {
+        for (let i = 0; i < max_words; i++) {
           new_text += `           |`;
         }
-        new_text += `\n|`;
+        new_text += '\n|';
         // Tableのalign設定の生成
-        for (let i = 0; i < lines[0].length; i++) {
+        for (let i = 0; i < words.length; i++) {
           if (type === 'left') {
             new_text += `:-----------|`;
           } else if (type === 'center') {
-            new_text += `-----------:|`;
-          } else if (type === 'right') {
             new_text += `:-----------:|`;
+          } else if (type === 'right') {
+            new_text += `-----------:|`;
           }
         }
-        new_text += `\n|`;
+        new_text += `\n`;
+        // Tableのセルを行ごとに生成
         for (let i = 0; i < lines.length; i++) {
-          let words = lines[i].split(/\s+/);
+          new_text += '|';
+          words = trimSpace(lines[i]).split(/\s+/);
           for (let j = 0; j < words.length; j++) {
             new_text += ` ${words[j]} |`;
           }
@@ -328,17 +361,25 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
 
   const readTxtFile = async (filePath: string) => {
     await window.electron.fs.existTxtFile(filePath).then(async (fileExists) => {
-      if (fileExists) {
-        const filedata = await window.electron.fs.readTxtFile(filePath);
-        setMemoContents(filedata);
-        await window.electron.makeMD
-          .makeMD(filedata)
-          .then((memoHTML: string) => {
-            setMemoHTML(sanitize(memoHTML));
-          });
-        setWatchMD(true);
-      } else {
-        console.error('File does not exist:', filePath);
+      try {
+        if (fileExists) {
+          const filedata = await window.electron.fs.readTxtFile(filePath);
+          setMemoContents(filedata);
+          // Filedataが選択されるまではinit=falseで右画面を空にしておく．
+          if (filedata !== '') {
+            setInit(true);
+          }
+          await window.electron.makeMD
+            .makeMD(filedata)
+            .then((memoHTML: string) => {
+              setMemoHTML(sanitize(memoHTML));
+            });
+          setWatchMD(true);
+        } else {
+          console.error('File does not exist:', filePath);
+        }
+      } catch (error) {
+        console.error('Error during file processing', error);
       }
     });
   };
@@ -355,13 +396,6 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
   const handleTableClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
     setOpenTP(!openTablePicker);
-    let textarea = document.getElementById('tarea') as HTMLTextAreaElement;
-    if (textarea) {
-      const selectionStart = textarea.selectionStart;
-      const selectionEnd = textarea.selectionEnd;
-      textarea.focus();
-      textarea.setSelectionRange(selectionStart, selectionEnd);
-    }
   };
 
   const handleCloseCP = (event: Event | React.SyntheticEvent) => {
@@ -375,6 +409,7 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
   };
 
   const handleCloseTP = (event: Event | React.SyntheticEvent) => {
+    event.stopPropagation();
     if (
       anchorRefColor.current &&
       anchorRefColor.current.contains(event.target as HTMLElement)
@@ -384,28 +419,35 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
     setOpenTP(false);
   };
 
-  const handleBlur = (event: any) => {
-    if (event._isPropagationStopped) {
+  const handleBlur = (event: React.FocusEvent<HTMLInputElement>) => {
+    if (event.isPropagationStopped()) {
       return;
     }
-    if (event.relatedTarget === document.getElementsByClassName('ribon-btn')) {
+    const popperButtons = document.querySelectorAll('.ribon-btn');
+    const popperContent = document.getElementById('popper-content');
+
+    if (
+      popperContent &&
+      Array.from(popperButtons).some((button) =>
+        button.contains(event.target),
+      ) &&
+      !popperContent.contains(event.target)
+    ) {
       return;
     }
     if (!event.currentTarget.contains(event.relatedTarget)) {
       setExitEditor(true);
       setTextSelected(false);
       setSelectedText('');
-      console.log('Blured');
     }
   };
 
-  const handleFocus = (event: any) => {
+  const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
     setExitEditor(false);
   };
 
   // MDプレビューを表示すると同時に，Txtファイルの編集を一旦確定させる．
   const previewMD = async () => {
-    console.log('previewMD');
     await window.electron.fs.writeTxtFile(txtFilePath, memoContents);
     await window.electron.makeMD.makeMD(memoContents).then(async (memoHTML) => {
       setWatchMD(!watchMD);
@@ -441,7 +483,7 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
           alignItems="center"
         >
           <Grid item>
-            {memoContents === '' ? (
+            {!init && memoContents === '' ? (
               <></>
             ) : (
               <Fade in={true} timeout={1000}>
@@ -567,39 +609,45 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
                   <CheckBoxIcon />
                 </IconButton>
               </ButtonGroup>
-              <Menu
-                anchorEl={anchorRefTable.current}
+              <Popper
                 open={openTablePicker}
-                onClose={handleCloseTP}
-                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-                className="tableMenu ribon-btn"
+                anchorEl={anchorRefTable.current}
+                role={undefined}
+                placement="bottom"
+                className="tableMenu"
+                id="popper-content"
               >
-                <Grid container>
-                  <Grid item>
-                    <MenuItem onClick={() => drawTable('left')}>
+                <ClickAwayListener onClickAway={handleCloseTP}>
+                  <ButtonGroup className="ribon ribon-btn">
+                    <IconButton
+                      className="ribon-btn"
+                      onClick={() => drawTable('left')}
+                    >
                       <FormatAlignLeftIcon />
-                    </MenuItem>
-                  </Grid>
-                  <Grid item>
-                    <MenuItem onClick={() => drawTable('center')}>
+                    </IconButton>
+                    <IconButton
+                      className="ribon-btn"
+                      onClick={() => drawTable('center')}
+                    >
                       <FormatAlignCenterIcon />
-                    </MenuItem>
-                  </Grid>
-                  <Grid item>
-                    <MenuItem onClick={() => drawTable('right')}>
+                    </IconButton>
+                    <IconButton
+                      className="ribon-btn"
+                      onClick={() => drawTable('right')}
+                    >
                       <FormatAlignRightIcon />
-                    </MenuItem>
-                  </Grid>
-                </Grid>
-              </Menu>
+                    </IconButton>
+                  </ButtonGroup>
+                </ClickAwayListener>
+              </Popper>
               <Menu
                 anchorEl={anchorRefColor.current}
                 open={openColorPicker}
                 onClose={handleCloseCP}
                 anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
                 transformOrigin={{ vertical: 'top', horizontal: 'center' }}
-                className="colorMenu ribon-btn"
+                className="colorMenu"
+                id="my-menu"
               >
                 <Grid container>
                   <Grid item>
@@ -666,7 +714,7 @@ const MemoMarkdown = ({ nowPdf, dirPath }: Props) => {
               />
             </div>
           </>
-        ) : memoContents === '' ? (
+        ) : init === false && memoContents === '' ? (
           <></>
         ) : (
           <Fade in={true} timeout={1000}>
