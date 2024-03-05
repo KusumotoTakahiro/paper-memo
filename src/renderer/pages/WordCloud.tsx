2484e3f330ffa738da449a0a97b9a7f9da12a8e0
KusumotoTakahiro <gantaka921@icloud.com>
2024-03-05 13:52:24 +0900

    [add] wordCloudの実装（途中）

--- src/renderer/pages/WordCloud.tsx
+++ src/renderer/pages/WordCloud.tsx
@@ -1,7 +1,80 @@
 import * as React from 'react';
 import { Grid } from '@mui/material';
 
+import { PDFMetaData } from '../../common/types';
+
 const WordCloud = () => {
+  const [dirPath, setDirPath] = React.useState<string>('');
+  const [pdfs, setPdfs] = React.useState<PDFMetaData[]>([]);
+
+  const readDirectory = async () => {
+    await window.electron.fs
+      .readFolderPDF(dirPath)
+      .then(async (pdfFiles) => {
+        let newPdfFiles: PDFMetaData[] = [];
+        let pdf: string;
+        let pdfFile: PDFMetaData;
+        for (let i = 0; i < pdfFiles.length; i++) {
+          pdf = pdfFiles[i];
+          await window.electron.PDFmetaData.getInfo(dirPath, pdf)
+            .then((metaData: PDFMetaData) => {
+              pdfFile = {
+                fileName: pdf,
+                pages: metaData['pages'],
+                fileSize: metaData['fileSize'],
+              };
+              newPdfFiles.push(pdfFile);
+            })
+            .catch((err) => {
+              console.error(err);
+            });
+        }
+        setPdfs(newPdfFiles);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const readTxtFile = async (filePath: string) => {
+    await window.electron.fs.existTxtFile(filePath).then(async (fileExists) => {
+      try {
+        if (fileExists) {
+          const filedata = await window.electron.fs.readTxtFile(filePath);
+        } else {
+          console.error('File does not exist:', filePath);
+        }
+      } catch (error) {
+        console.error('Error during file processing', error);
+      }
+    });
+  };
+
+  const joinTexts = async () => {
+    let pdf: PDFMetaData;
+    for (let i = 0; i < pdfs.length; i++) {
+      pdf = pdfs[i];
+      const fl = pdf.fileName.length;
+      const filePath =
+        dirPath + '\\Memo\\' + pdf.fileName.slice(0, fl - 4) + '.txt';
+    }
+  };
+
+  const refleshWindow = () => {
+    window.electron.electronStore.getlist().then((list) => {
+      window.electron.electronStore.getSelectedIndex().then((idx) => {
+        setDirPath(list[idx]);
+        if (dirPath !== '') {
+          readDirectory();
+        }
+      });
+    });
+  };
+
+  React.useEffect(() => {
+    refleshWindow();
+  }, [dirPath]);
+
   return (
     <>
       <Grid container spacing={2}>
